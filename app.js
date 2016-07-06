9b4f566f6c2530887c9c4cd5c37ba63857d67dd9
Dan Arnold <dan.arnold@gmail.com>
2016-07-06 19:51:24 +0100

    Added nodemon to gulp

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ var express = require('express');
 
 var app = express();
 
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
 app.get('/', function(req, res) {
-    res.send('Hello world');
+    res.send('Hello World');
 });
 
 app.get('/books', function(req, res) {
-    res.send('Hello books');
+    res.send('Hello Books');
 });
 
 app.listen(port, function(err) {
