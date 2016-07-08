41a267ac34d3f13959424e95de620da1e974f3ca
Dan Arnold <dan.arnold@gmail.com>
2016-07-08 02:46:37 +0100

    Starting auth work

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv');
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 
@@ -32,14 +33,19 @@ var nav = [{
 }];
 var bookRouter = require('./src/routes/bookRoutes')(nav);
 var adminRouter = require('./src/routes/adminRoutes')(nav);
+var authRouter = require('./src/routes/authRoutes')(nav);
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 var bookRouter = require('./src/routes/bookRoutes')(nav);
 app.use('/books', bookRouter);
 app.use('/admin', adminRouter);
+app.use('/auth', authRouter);
 
 app.get('/', function(req, res) {
     res.render('index', {
