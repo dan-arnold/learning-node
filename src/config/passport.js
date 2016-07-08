1f18ebc9f157214d503d52abe7bc278d2dfb09d1
Dan Arnold <dan.arnold@gmail.com>
2016-07-08 23:40:15 +0100

    Moving book logic into controller

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,14 +1,14 @@
 var passport = require('passport');
 
-module.exports = function(app) {
+module.exports = function (app) {
     app.use(passport.initialize());
     app.use(passport.session());
 
-    passport.serializeUser(function(user, done) {
+    passport.serializeUser(function (user, done) {
         done(null, user);
     });
 
-    passport.deserializeUser(function(user, done) {
+    passport.deserializeUser(function (user, done) {
         done(null, user);
     });
 
--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -29,16 +29,16 @@ var books = [{
     read: false
 }];
 
-var router = function(nav) {
+var router = function (nav) {
 
     adminRouter.route('/addBooks')
-        .get(function(req, res) {
+        .get(function (req, res) {
             var url = 'mongodb://localhost:27017/libraryApp';
 
-            mongodb.connect(url, function(err, db) {
+            mongodb.connect(url, function (err, db) {
                 var collection = db.collection('books');
                 collection.insertMany(books,
-                    function(err, results) {
+                    function (err, results) {
                         res.send(results);
                         db.close();
                     }
